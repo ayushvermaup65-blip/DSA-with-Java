903d22ec77c6feaa3eaec49deac8225ed46a71e9
ayushvermaup65-blip <ayushvermaup65@gmail.com>
2026-06-01 02:29:18 +0530

    New Pattern Problem

--- src/Patterns/Pattern8.java
+++ src/Patterns/Pattern8.java
@@ -4,6 +4,25 @@ public class Pattern8 {
     static void main() {
         int n=4;
 
-        for(int row   )
+        for(int row=1; row<=n; row++){
+            for(int col=1; col<=6; col++){
+                if(row==1 || row==n){
+                    System.out.print("* ");
+                }
+                else{
+                    if(col==1 || col==6){
+                        System.out.print("* ");
+                    }
+                        else{
+                            System.out.print("  ");
+                        }
+                    }
+                }
+                System.out.println();
+            }
+        }
     }
-}
+//                  * * * * * *
+//                  *         *
+//                  *         *
+//                  * * * * * *
