3bbaf362ed8a9c9ef35f46c557520707e551ad7f
ayushvermaup65-blip <ayushvermaup65@gmail.com>
2026-06-02 04:45:05 +0530

    learned java methods

--- src/Methods.java
+++ src/Methods.java
@@ -1,4 +1,17 @@
-package PACKAGE_NAME;
-
 public class Methods {
+    //declaration/definition
+    static void print2KaTable(){
+        for(int i=1; i<=10; i++){
+            int ans = 2*i;
+            System.out.println("2 * " + i + " = " + ans);
+        }
+
+    }
+
+    static void main() {
+        System.out.println("hi");
+        print2KaTable(); //method call
+        System.out.println("bye");
+    }
+
 }
