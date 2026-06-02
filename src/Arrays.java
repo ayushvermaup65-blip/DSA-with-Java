c148f7f471afec3aa9de953e764f950d6f78e631
ayushvermaup65-blip <ayushvermaup65@gmail.com>
2026-06-03 03:41:20 +0530

    Learned Arrays

---
+++ src/Arrays.java
@@ -0,0 +1,42 @@
+import java.util.Scanner;
+
+public class Arrays {
+    static void main() {
+
+        int arr[] = new int [3];
+        int n = arr.length;
+        Scanner sc = new Scanner(System.in);
+        for (int i=0; i<n; i++){
+            System.out.println("Enter input for index: " + i);
+            arr[i] =sc.nextInt();
+        }
+
+        //prints
+        System.out.println("Your array contains: ");
+        for(int val: arr){
+            System.out.println(val);
+        }
+//        //declaration
+//        int arr[];
+//        //allocation
+//        arr = new int [5];
+//        //initialization
+//        int brr [] = {10,20,30};
+//
+//        int n = brr.length;
+//       for (int index=0; index<=n-1; index++){
+//           System.out.println(brr[index]);
+//        }
+//        //           OR
+//        for(int val: brr){
+//            System.out.println(val);
+//        }
+
+//        System.out.println("Value at 0 index: " + brr[0]);
+//        System.out.println("Value at 1 index: " + brr[1]);
+//        System.out.println("Value at 2 index: " + brr[2]);
+
+
+
+    }
+}
