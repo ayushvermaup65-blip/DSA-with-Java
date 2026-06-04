3ab9207a73c51111acdaa31fbafa38a4254beeaf
ayushvermaup65-blip <ayushvermaup65@gmail.com>
2026-06-05 04:04:11 +0530

    Added StringBasics Program

---
+++ src/Strings/StringBasics.java
@@ -0,0 +1,9 @@
+package Strings;
+
+public class StringBasics {
+    static void main() {
+        String firstName = "Ayush";
+        String lastName = new String("Verma");
+        System.out.println(firstName + " " + lastName);
+    }
+}
