21a32ab13cdeccc97171c93980eb450c3cd48090
ayushvermaup65-blip <ayushvermaup65@gmail.com>
2026-06-06 03:05:45 +0530

    Understood String methods

--- src/Strings/StringBasics.java
+++ src/Strings/StringBasics.java
@@ -2,8 +2,16 @@ package Strings;
 
 public class StringBasics {
     static void main() {
-        String firstName = "Ayush";
-        String lastName = new String("Verma");
-        System.out.println(firstName + " " + lastName);
+//        String firstName = "Ayush";
+//        String lastName =  new String("Verma");
+//        System.out.println(firstName + " " + lastName);
+//        System.out.println(firstName.length());
+//        System.out.println(firstName.charAt(1));
+//        firstName = firstName.concat(lastName);
+//        System.out.println(" " + firstName);
+
+        String name = "Rana"; // Rana
+//      name[0] = "b";
+        name = "Bana"; // Bana
     }
 }
