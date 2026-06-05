8224646f06efb440bacf170572052005c89bd929
ayushvermaup65-blip <ayushvermaup65@gmail.com>
2026-06-06 03:51:57 +0530

    Understood string methods

--- src/Strings/StringBasics.java
+++ src/Strings/StringBasics.java
@@ -1,5 +1,7 @@
 package Strings;
 
+import java.util.Scanner;
+
 public class StringBasics {
     static void main() {
 //        String firstName = "Ayush";
@@ -10,8 +12,32 @@ public class StringBasics {
 //        firstName = firstName.concat(lastName);
 //        System.out.println(" " + firstName);
 
-        String name = "Rana"; // Rana
-//      name[0] = "b";
-        name = "Bana"; // Bana
+//        String name = "Rana"; // Rana
+//        name[0] = "b";
+//        name = "Bana"; // Bana
+
+//          String name1 = "Ayush";
+//          String name2 = "AyUsh";
+//        System.out.println(name1.equalsIgnoreCase(name2));
+//          if (name1 == name2){
+//              System.out.println("both strings are equal");
+//          }
+//          else{
+//              System.out.println("both strings are not equal");
+//          }
+
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Provide the string content: ");
+        String str = sc.nextLine();
+        System.out.println("Value of nextLine() : " + str);
+
+
+        System.out.println("Provide the string content: ");
+        String str2 = sc.next();
+        System.out.println("Value of next() : " + str2);
+
+
+
     }
 }
---
+++ src/Strings/StringMethods.java
@@ -0,0 +1,36 @@
+package Strings;
+
+public class StringMethods {
+    static void main() {
+
+//        String str = "Ayush";
+//        System.out.println(str.length()); // 5
+//        System.out.println(str.charAt(0)); // A
+//
+//        String name = "AYUSH";
+//        System.out.println(str.equals(name)); // false
+//        System.out.println(str.equalsIgnoreCase(name)); // true
+
+        //  empty -> length = 0
+        // blank -> empty or sirf spaces h string
+//        String str1 = "  ";
+//        System.out.println(str1.length()); // 2
+//        System.out.println(str1.isEmpty()); // false
+//        System.out.println(str1.isBlank());  // true
+
+//        String name = "   Ayush   ";
+//        System.out.println(name.length()); // 11
+//        name = name.trim();
+//        System.out.println(name.length()); // 5
+
+
+        String name = "Ayush";
+        System.out.println(name.toUpperCase());
+        String str = "LOVE";
+        System.out.println(str.toLowerCase());
+
+
+
+
+    }
+}
