416b2efe723617ff59da22215236abdeaf03bf4f
ayushvermaup65-blip <ayushvermaup65@gmail.com>
2026-06-06 15:53:59 +0530

    Understood all string methods

--- src/Strings/StringMethods.java
+++ src/Strings/StringMethods.java
@@ -24,13 +24,61 @@ public class StringMethods {
 //        System.out.println(name.length()); // 5
 
 
-        String name = "Ayush";
-        System.out.println(name.toUpperCase());
-        String str = "LOVE";
-        System.out.println(str.toLowerCase());
+//        String name = "Ayush";
+//        System.out.println(name.toUpperCase()); // AYUSH
+//        String str = "LOVE";
+//        System.out.println(str.toLowerCase()); // love
+
+//        String str = "My Name is Ayush Verma";
+        //0 -> M
+        // 1 -> y
+        //2 -> space and so on
+        //begin Index -> 3 -> inclusive
+        //end Index -> 6 -> exclusive
+//        System.out.println(str.substring(3,7)); // Name
+//        System.out.println(str.contains("Ayush")); // true
+
+//        int num = 5123;
+//        String str = String.valueOf(num);
+//        System.out.println(num+1); // 5124
+//        System.out.println(str + 1); // 51231
+
+//        String name = "Rana Ayush Verma";
+//        System.out.println(name.startsWith("Rana")); // true
+//        System.out.println(name.endsWith("Ayush")); // false
+
+
+//        String name = "Ayush";
+//        char[] crr = name.toCharArray();
+//
+//        //print the char array
+//        for (char ch: crr){
+//            System.out.println("Value of char: " + ch); //   Value of char: A
+////                                                           Value of char: y
+////                                                           Value of char: u
+////                                                           Value of char: s
+////                                                           Value of char: h
+//
+//        }
+
+        //split -> output -> String array
+//        String input = "My,name,is,ayush,verma";
+//        String[] words = input.split(",");
+//        for (String str: words){
+//            System.out.println(str);
+//                    My
+//                    name
+//                    is
+//                    ayush
+//                    verma
 
 
 
+//        String name = "Ayush";
+//        name = name.replace('A','M');
+//        System.out.println(name); // Myush
+
+        }
 
     }
-}
+
