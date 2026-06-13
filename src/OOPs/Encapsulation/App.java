c9c51c2c449138dd37f425b06239046cac7cb4cd
ayushvermaup65-blip <ayushvermaup65@gmail.com>
2026-06-13 14:46:01 +0530

    restored files and Encapsulation package

---
+++ src/OOPs/Encapsulation/App.java
@@ -0,0 +1,44 @@
+package OOPs.Encapsulation;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+//        Student A = new Student();
+//        A.id = 1;
+//        A.name = "Ayush";
+//        A.age = 14;
+//        A.nos = 5;
+//        System.out.println(A.name);
+//        System.out.println(A.age);
+//        System.out.println(A.id);
+//        System.out.println(A.nos);
+//
+//        A.bunk();
+//        A.study();
+//        A.sleep();
+
+        // Parameterized Ctor
+        Student A = new Student(1, 12, "Rahul", 3, "Tina");
+        System.out.println(A.getAge());
+
+        A.setAge(6765);
+
+        System.out.println(A.getAge());
+        //System.out.println(A.getName());
+//        System.out.println(A.getAge());
+//        System.out.println(A.getId());
+//        System.out.println(A.getNos());
+//        System.out.println(A.getGf());
+
+//        A.bunk();
+//        A.study();
+//        A.sleep();
+
+        // copy ctor
+//       Student B = new Student(A);
+//        System.out.println(B.name);
+//        System.out.println(B.age);
+//        System.out.println(B.id);
+//        System.out.println(B.nos);
+//        B.sleep();
+    }
+}
---
+++ src/OOPs/Encapsulation/Student.java
@@ -0,0 +1,68 @@
+package OOPs.Encapsulation;
+
+public class Student {
+    // Attributes
+    private int id;
+    private int age;
+    private String name;
+    private int nos;
+    private String gf;
+
+    public String getName(){
+        return this.name;
+    }
+
+    public int getAge(){
+        return this.age;
+    }
+
+    public void setAge(int a){
+        //extra layer of authentication
+        if(age < 100) {
+            this.age = a;
+        }
+        else {
+        return;
+    }}
+
+    //Default ctor // attribute -> garbage
+    public Student(){
+        System.out.println("Student Default ctor Called");
+    }
+
+    //Parameterized ctor
+    public Student(int id, int age, String name, int nos, String gf){
+        System.out.println("Student Parameterized ctor Called");
+        this.id = id;
+        this.age = age  ;
+        this.name = name;
+        this.nos = nos;
+        this.gf = gf;
+    }
+
+    //copy ctor
+    public Student(Student srcobj) {
+        System.out.println("Student Copy ctor Called");
+        this.id = srcobj.id;
+        this.age = srcobj.age;
+        this.name = srcobj.name;
+        this.nos = srcobj.nos;
+    }
+
+    // Methods / Behaviours
+    public void study(){
+        System.out.println(name + " Studying");
+    }
+
+    public void sleep(){
+        System.out.println(name + " sleeping");
+    }
+
+    public void bunk(){
+        System.out.println(name + " bunking");
+    }
+
+    private void gfChatting(){
+        System.out.println(name + "Chatting with gf");
+    }
+}
