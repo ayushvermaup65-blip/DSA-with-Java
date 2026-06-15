0a44602faf0355888a965ac076f41d9f9f3b6b96
ayushvermaup65-blip <ayushvermaup65@gmail.com>
2026-06-16 01:07:24 +0530

    Learned about Upcasting and Downcasting

--- src/OOPs/Polymorphism/Main.java
+++ src/OOPs/Polymorphism/Main.java
@@ -7,7 +7,7 @@ public class Main {
 //        System.out.println(c.add(2,3,4));
 //        System.out.println(c.add(2,3,4,5.6));
 
-        // Runtime Polymorphism
+//      Runtime Polymorphism
         Circle c = new Circle();
         doDrawingStuff(c);
 
@@ -17,9 +17,22 @@ public class Main {
         Shape s = new Shape();
         doDrawingStuff(s);
 
+        //Downcasting
+//        Circle c = new Circle();
+//        doDrawingStuff(c);
+
+//        Rect r = new Rect();
+//        doDrawingStuff(r);
+
     }
 
-    public static void doDrawingStuff(Shape s){
-        s.draw(); //  iska meaning hai ki Shape class ka draw method call hona chahiye , par ye Circle class ke draw method ko call kar rha hai
-    }
+    public static void doDrawingStuff(Shape s){ //parameter me Shape ka object s likha hai jo apne saare childs ke objects ko hold karega
+        s.draw(); //ye ek Polymorphic method hai
+        //  iska meaning hai ki Shape class ka draw method call hona chahiye ,
+        // par ye apne child class (Circle class ke draw method ko call kar rha hai)
+        // so this is called Upcasting
+//        Circle c = (Circle)s; // Downcasting
+//        c.draw();
+     }
 }
+ 
