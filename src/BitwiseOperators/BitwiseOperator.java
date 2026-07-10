0b5ce763750c9fb3db1d48388088bfcea0e8ca03
ayushvermaup65-blip <ayushvermaup65@gmail.com>
2026-07-11 03:49:51 +0530

    Program on bitwise operator

---
+++ src/BitwiseOperators/BitwiseOperator.java
@@ -0,0 +1,14 @@
+package BitwiseOperators;
+
+public class BitwiseOperator {
+    static void main() {
+
+        int a = 5;
+        int b = 6;
+        System.out.println(a & b);
+        System.out.println(a | b);
+        System.out.println(a ^ b);
+        System.out.println(~a);
+
+    }
+}
