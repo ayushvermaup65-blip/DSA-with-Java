a2e69a94da67b3e29eb2753eeefae8204f3ce3b1
ayushvermaup65@gmail.com <user.ayushvermaup65@gmail.com>
2026-07-15 22:45:09 +0530

    Solved question to Shift Array Elements by 1 Position

--- src/Lecture28/ArrayQuestions2.java
+++ src/Lecture28/ArrayQuestions2.java
@@ -18,6 +18,25 @@ public class ArrayQuestions2 {
         return arr;
     }  // T.C. -> O(n), S.C. -> O(1)
 
+
+
+    // Question 2: Shift array elements by 1 position
+    static void shiftBy1(int []arr){
+        int size = arr.length;
+
+        // last element to temp me store karo
+        int temp = arr[size-1];
+
+        //value ko 1 place aage shift karo
+        for (int i = size-1; i>0; i--){
+            arr[i] = arr[i-1];
+        }
+
+        // temp ko 0 index pe rakh do
+        arr[0] = temp;
+    }  // T.C. -> O(n), S.C. -> O(1)
+
+
     static void main(String[] args) {
 
 
@@ -28,5 +47,14 @@ public class ArrayQuestions2 {
             System.out.println(num);
         }
 
+
+
+        // Question 2: Shift array elements by 1 position
+        int arr2[] = {1,2,3,4,5};
+        shiftBy1(arr2);
+        for (int a: arr2){
+            System.out.println(a + " ");
+        }
+        System.out.println();
     }
 }
