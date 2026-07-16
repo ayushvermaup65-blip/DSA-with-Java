02e4e9c5d6f3c9a95412b3fc69bd528d4231affe
ayushvermaup65@gmail.com <user.ayushvermaup65@gmail.com>
2026-07-17 01:42:29 +0530

    testing

--- src/Lecture28/Homework2.java
+++ src/Lecture28/Homework2.java
@@ -16,7 +16,7 @@ public class Homework2 {
         }
 
         // Copy back to original array
-        for (int i = 0; i < size; i++){
+        for(int i = 0; i < size; i++){
             arr[i] = temp[i];
         }
         return arr;
